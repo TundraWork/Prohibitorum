f244634361ac41babb52084e94754bffa48215f7
Jesse Cheng <jesse.cheng@bytedance.com>
2026-06-17 16:37:42 +1000

    build(webui): rebuild SPA for server-validated login redirect

---
+++ pkg/webui/dist/assets/Skeleton.vue_vue_type_script_setup_true_lang-DRLuz-lS.js
@@ -0,0 +1,1 @@
+import{a as s}from"./index-Dn8FM2zZ.js";import{d as n,b as o,x as r,f as t,o as l}from"./index-Nj7Hbs8-.js";const u=n({__name:"Skeleton",props:{class:{type:[Boolean,null,String,Object,Array]}},setup(e){const a=e;return(c,p)=>(l(),o("div",{"data-slot":"skeleton",class:r(t(s)("animate-pulse rounded-md bg-primary/10",a.class))},null,2))}});export{u as _};
---
+++ pkg/webui/dist/assets/useFormControl-C3NcLlk7.js
@@ -0,0 +1,1 @@
+import{f as t}from"./useForwardExpose-NP_sC85j.js";import{c as e,a0 as m}from"./index-Nj7Hbs8-.js";function n(o){return e(()=>{var r;return m(o)?!!((r=t(o))!=null&&r.closest("form")):!0})}export{n as u};
