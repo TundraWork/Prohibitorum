2f8787e30224f6b89dea555bdd1e377a1c2acac6
Tundra Work <tundrawork@gmail.com>
2026-06-24 20:25:36 +1000

    chore(launchpad): rebuild embedded SPA bundle

--- pkg/webui/dist/assets/useFormControl-CZeLwFDP.js
+++
@@ -1,1 +0,0 @@
-import{f as t}from"./useForwardExpose-DcHQoILL.js";import{c as e,af as m}from"./index-BS8LbTZH.js";function f(o){return e(()=>{var r;return m(o)?!!((r=t(o))!=null&&r.closest("form")):!0})}export{f as u};
