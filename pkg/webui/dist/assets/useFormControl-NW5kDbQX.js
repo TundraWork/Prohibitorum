8b5df884ef20bb3184d2fba0dd000bc468179722
Jesse Cheng <jesse.cheng@bytedance.com>
2026-06-24 14:34:33 +1000

    chore(launchpad): rebuild embedded SPA bundle (homepage refinement)

---
+++ pkg/webui/dist/assets/useFormControl-NW5kDbQX.js
@@ -0,0 +1,1 @@
+import{f as t}from"./useForwardExpose-5uMqX97y.js";import{c as e,af as m}from"./index-DadXQDjb.js";function f(o){return e(()=>{var r;return m(o)?!!((r=t(o))!=null&&r.closest("form")):!0})}export{f as u};
