bdc61dbcb97b18516f0a243b45ebf4f3a3f9a398
Tundra Work <tundrawork@gmail.com>
2026-07-22 23:42:54 +1000

    build: refresh embedded dashboard bundle

---
+++ pkg/webui/dist/assets/useFormControl-QveOtHpv.js
@@ -0,0 +1,1 @@
+import{f as t}from"./useForwardExpose-DE2e_Lf2.js";import{c as e,ap as m}from"./index-BhiLBJRy.js";function n(o){return e(()=>{var r;return m(o)?!!((r=t(o))!=null&&r.closest("form")):!0})}export{n as u};
